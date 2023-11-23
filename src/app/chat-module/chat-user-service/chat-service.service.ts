937e5e86c7139cc507d9433638af39a7ece050f2
Ricardo Diaz Zepeda <82178419+RenionDxD@users.noreply.github.com>
2023-11-23 16:39:32 -0600

    panel de sugerencias, textos de color, botones de like

--- src/app/chat-module/chat-user-service/chat-service.service.ts
+++ src/app/chat-module/chat-user-service/chat-service.service.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable,of } from 'rxjs';
+import { map,catchError } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class ChatServiceService {
 
   private apiUrl = 'http://127.0.0.1:5051/chatbot/preguntas'; // Reemplaza con la URL de tu API
-
+  private apiUrl2 = 'http://127.0.0.1:5051/chatbot/Online';
+  private apiUrl3 = 'http://127.0.0.1:5051/chatbot/send_review';
   constructor(private http: HttpClient) { }
 
   enviarMensaje(mensaje: string): Observable<any> {
     const body = { "pregunta": mensaje };
     // Realiza una solicitud POST a la API con el mensaje
     return this.http.post<any>(this.apiUrl, body );
+  }
+
+  checkServerStatus() {
+    return this.http.get(this.apiUrl2);
+  }
+
+  sendReview(review: boolean, respuesta: string) {
+    const body = { "respuesta": respuesta,
+                   "review":review };
+    return this.http.post<any>(this.apiUrl3, body );
   }
 }
--- src/app/chat-module/chat-user/chat-user.component.ts
+++ src/app/chat-module/chat-user/chat-user.component.ts
@@ -9,29 +9,92 @@ import { ChatServiceService } from '../chat-user-service/chat-service.service'
 export class ChatUserComponent {
 
   mensaje: string = '';
-  mensajes: { texto: string, tipo: string }[] = [];
+  pregunta: string = '';
+  mensajes: { texto: string, tipo: string, color?: string }[] = [];
+  preguntas: { pregunta: string, tipo: string }[] = [];
+  color: string = '';
+ 
+  serverStatus: boolean = false;
+
 
   constructor(private chatService: ChatServiceService) {}
 
-  
+
+
+
   loading: boolean = false; 
+  likeStatus: string = '';
   
   enviarMensaje() {
     this.loading = true;
     this.chatService.enviarMensaje(this.mensaje).subscribe(
       (respuesta) => {
+        this.preguntas = [];
+
         this.mensajes.push({ texto: this.mensaje, tipo: 'enviado' });
-        this.mensajes.push({ texto: respuesta.solucion, tipo: 'recibido' });
+        const colorDelMensajeRecibido = respuesta.color_solucion ? '' : '#d75b5d';
+        this.mensajes.push({ texto: respuesta.solucion, tipo: 'recibido', color: colorDelMensajeRecibido });
+        // Agregar mensajes similares en tarjetas
+        this.preguntas.push({ pregunta: respuesta.similares, tipo: 'preguntas' });
         this.mensaje = '';
+        this.pregunta = '';
         this.loading = false;// Realiza acciones adicionales si es necesario
       },
       (error) => {
         this.loading = false;
         console.error('Error al enviar mensaje', error);
       }
-    );
-    
+    ); 
   }
+
+  enviarPregunta(pregunta: string){
+    this.loading = true;
+    this.chatService.enviarMensaje(pregunta).subscribe(
+      (respuesta) => {
+        this.preguntas = [];
+        this.mensajes.push({ texto: pregunta, tipo: 'enviado' });
+        this.mensajes.push({ texto: respuesta.solucion, tipo: 'recibido' });
+        // Agregar mensajes similares en tarjetas
+        this.preguntas.push({ pregunta: "", tipo: 'preguntas' });
+        this.mensaje = '';
+        this.pregunta = '';
+        this.loading = false;// Realiza acciones adicionales si es necesario
+      },
+      (error) => {
+        this.loading = false;
+        console.error('Error al enviar mensaje', error);
+      }
+    ); 
+  }
+
+  showAlert(reaction: boolean,mensaje:string): void {
+    this.chatService.sendReview(reaction,mensaje).subscribe()
+  }
+
+
+
+  ngOnInit() {
+    this.chatService.checkServerStatus().subscribe(
+      (response: any) => {
+        this.serverStatus = response.status === 'online';
+        // Resto del código según tus necesidades
+      },
+      error => {
+        console.error('Error al obtener el estado del servidor:', error);
+        // Manejo de error según tus necesidades
+      }
+    );
+  }
+
+
+ 
+
+  
+
+
+
+
+
 
   
 
